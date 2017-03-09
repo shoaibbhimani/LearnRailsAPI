5c3d9efd580f03751a32076072226fc0b121ffed
Shoaib Bhimani <shoaibbhimani1392@gmail.com>
2017-03-09 20:46:07 +0530

    Adds user firsts specs

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :person do
-    
+    email {  FFaker::Internet.email }
+    password "!2123123123"
+    password_confirmation "!2123123123"
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @person = FactoryGirl.build(:person) }
+  subject { @person }
+
+  it {  should respond_to(:email) }
+  it {  should respond_to(:password) }
+  it {  should respond_to(:password_confirmation) }
+
+  it {  should be_valid }
+
 end
