972c15a11eb8cfcef6dffbc4a633af33496af4ce
Shoaib Bhimani <shoaibbhimani1392@gmail.com>
2017-03-09 20:50:49 +0530

    Adds shoulda matchers for spec refactors

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -11,3 +11,11 @@ describe Person do
   it {  should be_valid }
 
 end
+
+describe "when email is not present" do
+  it { should  validate_presence_of(:email) }
+  it { should  validate_uniqueness_of(:email) }
+  it { should  validate_confirmation_of(:password) }
+  it { should  allow_value('example@domain.com').for(:email) }
+end
+
